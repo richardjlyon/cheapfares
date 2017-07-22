635180c04b58d58f89ea54882a69ca759b5c8df9
richardjlyon <richard@richardlyon.net>
2017-07-22 16:49:28 +0100

    Enforce RSpec Expecations syntax

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,10 @@
 require "bundler/setup" # ensure we're loading Gemfile defined gems
 require File.dirname(__FILE__) + '/../lib/cheapfares.rb'
+
+# Enforce RSpec Expectation syntax
+RSpec.configure do |config|
+  # ...
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
+end
